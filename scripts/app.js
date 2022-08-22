3a2fa9d6c462f8858402a731c8eade361d68ebee
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-08-22 20:47:24 +0600

    Calculate All Cost Complete.

--- scripts/app.js
+++ scripts/app.js
@@ -16,6 +16,14 @@ const costForCoachField = document.getElementById('cost-for-coach');
 const totalPlayerCostField = document.getElementById('total-player-cost');
 const totalCostField = document.getElementById('total-expenses');
 
+function resetAllField() {
+	costPerPlayerField.value = '';
+	costForManagerField.value = '';
+	costForCoachField.value = '';
+	totalPlayerCostField.value = '';
+	totalCostField.value = '';
+}
+
 /**
  * @description This function will calculate the selected players.
  * @author Shekh Rasel Masrur Ahmmad
@@ -54,13 +62,28 @@ function addPlayerToTheList(event) {
  * @description This function will take the input field ID as parameter and return the value by converting that to an float value.
  * @author Shekh Rasel Masrur Ahmmad
  * @date 22/08/2022
- * @param {*} inputFieldID
+ * @param {String} inputFieldID
+ * @returns {Number}
  */
 function getNumberValueFormInputField(inputFieldID) {
 	const inputField = document.getElementById(inputFieldID);
 	const inputFieldValueString = inputField.value;
 	const inputFieldValueNumber = parseFloat(inputFieldValueString);
 	return inputFieldValueNumber;
+}
+
+/**
+ * @description
+ * @author Shekh Rasel Masrur Ahmmad
+ * @date 22/08/2022
+ * @param {String} elementFieldID
+ * @returns {Number}
+ */
+function getNumberValueFromElementField(elementFieldID) {
+	const elementField = document.getElementById(elementFieldID);
+	const elementFieldValueString = elementField.innerText;
+	const elementFieldValueNumber = parseFloat(elementFieldValueString);
+	return elementFieldValueNumber;
 }
 
 /**
@@ -77,11 +100,26 @@ function calculatePlayerCost(totalPlayer, costPerPlayer) {
 }
 
 /**
+ * @description
+ * @author Shekh Rasel Masrur Ahmmad
+ * @date 22/08/2022
+ * @param {Number} playerCost
+ * @param {Number} costForManager
+ * @param {Number} costForCoach
+ * @returns {Number}
+ */
+function calculateTotalCost(playerCost, costForManager, costForCoach) {
+	const totalCost = (playerCost + costForManager + costForCoach).toFixed(2);
+	return parseFloat(totalCost);
+}
+
+/**
  * Select Button functionality of Players Card
  */
 for (const selectPlayerButton of selectPlayersButtons) {
 	selectPlayerButton.addEventListener('click', function (event) {
 		addPlayerToTheList(event);
+		resetAllField();
 	});
 }
 
@@ -103,4 +141,33 @@ playerCostCalculateBtn.addEventListener('click', function (event) {
 		totalPlayerCost = calculatePlayerCost(totalPlayer, costPerPlayer);
 		totalPlayerCostField.innerText = totalPlayerCost;
 	}
+});
+
+/**
+ * Total Cost Calculation
+ */
+totalCostCalculationBtn.addEventListener('click', function (event) {
+	const costForManager = getNumberValueFormInputField('cost-for-manager');
+	const costForCoach = getNumberValueFormInputField('cost-for-coach');
+	const playerCost = getNumberValueFromElementField('total-player-cost');
+	let totalCost = 0;
+
+	if (playerCost === 0) {
+		alert('Calculate Player Cost First');
+	} else if (
+		isNaN(costForManager) ||
+		isNaN(costForCoach) ||
+		costForManager <= 0 ||
+		costForCoach <= 0
+	) {
+		alert('Please Provide valid cost.');
+	} else {
+		totalCost = calculateTotalCost(
+			playerCost,
+			costForManager,
+			costForCoach
+		);
+	}
+	console.log(totalCost);
+	totalCostField.innerText = totalCost;
 });
