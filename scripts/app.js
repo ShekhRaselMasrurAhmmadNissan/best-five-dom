2c33b379427ff10721eadd5bbda820878913bfee
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-08-22 18:14:17 +0600

    Player count function created.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,17 @@
 const selectPlayersButtons = document.getElementsByClassName('select-player');
 
 /**
+ * @description This function will calculate the selected players.
+ * @author Shekh Rasel Masrur Ahmmad
+ * @date 22/08/2022
+ * @returns {*}
+ */
+function calculateSelectedPlayers() {
+	const allSelectedPlayers = document.querySelectorAll('li');
+	return allSelectedPlayers.length;
+}
+
+/**
  * @description This function will add a player to the list after clicking on the Select button.
  * @author Shekh Rasel Masrur Ahmmad
  * @date 22/08/2022
@@ -10,11 +21,11 @@ function addPlayerToTheList(event) {
 	const selectPlayerButton = event.target;
 	const playerName = event.target.parentNode.parentNode.children[0].innerText;
 	const allPlayerList = document.getElementById('players-list');
-	const allSelectedPlayers = document.querySelectorAll('li');
+	const selectedPlayers = calculateSelectedPlayers();
 	const li = document.createElement('li');
-	console.log(allSelectedPlayers.length);
-	if (allSelectedPlayers.length < 5) {
-		li.innerHTML = `${allSelectedPlayers.length + 1}. ${playerName}`;
+	console.log(selectedPlayers);
+	if (selectedPlayers < 5) {
+		li.innerHTML = `${selectedPlayers + 1}. ${playerName}`;
 		allPlayerList.appendChild(li);
 
 		selectPlayerButton.setAttribute('disabled', true);
