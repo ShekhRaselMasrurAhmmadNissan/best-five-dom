9fa3eae08a634455f12bbdda273302254283dc49
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-08-22 19:48:12 +0600

    Calculated Player Expenses.

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,20 @@
+/**
+ * All The Buttons
+ */
 const selectPlayersButtons = document.getElementsByClassName('select-player');
+const playerCostCalculateBtn = document.getElementById('player-cost-calculate');
+const totalCostCalculationBtn = document.getElementById('all-cost-calculate');
+/**
+ * All The Input field
+ */
+const costPerPlayerField = document.getElementById('per-player-cost');
+const costForManagerField = document.getElementById('cost-for-manager');
+const costForCoachField = document.getElementById('cost-for-coach');
+/**
+ * All the Results Field
+ */
+const totalPlayerCostField = document.getElementById('total-player-cost');
+const totalCostField = document.getElementById('total-expenses');
 
 /**
  * @description This function will calculate the selected players.
@@ -34,8 +50,57 @@ function addPlayerToTheList(event) {
 	}
 }
 
+/**
+ * @description This function will take the input field ID as parameter and return the value by converting that to an float value.
+ * @author Shekh Rasel Masrur Ahmmad
+ * @date 22/08/2022
+ * @param {*} inputFieldID
+ */
+function getNumberValueFormInputField(inputFieldID) {
+	const inputField = document.getElementById(inputFieldID);
+	const inputFieldValueString = inputField.value;
+	const inputFieldValueNumber = parseFloat(inputFieldValueString);
+	return inputFieldValueNumber;
+}
+
+/**
+ * @description
+ * @author Shekh Rasel Masrur Ahmmad
+ * @date 22/08/2022
+ * @param {Number} totalPlayer
+ * @param {Number} costPerPlayer
+ * @returns {Number}
+ */
+function calculatePlayerCost(totalPlayer, costPerPlayer) {
+	const playerCost = (totalPlayer * costPerPlayer).toFixed(2);
+	return parseFloat(playerCost);
+}
+
+/**
+ * Select Button functionality of Players Card
+ */
 for (const selectPlayerButton of selectPlayersButtons) {
 	selectPlayerButton.addEventListener('click', function (event) {
 		addPlayerToTheList(event);
 	});
 }
+
+/**
+ * Player Cost Calculation
+ */
+playerCostCalculateBtn.addEventListener('click', function (event) {
+	const costPerPlayer = getNumberValueFormInputField('per-player-cost');
+
+	const totalPlayer = calculateSelectedPlayers();
+	let totalPlayerCost = 0;
+
+	if (totalPlayer === 0) {
+		alert('You have to select at least one player to calculate cost.');
+	} else if (isNaN(costPerPlayer) || costPerPlayer <= 0) {
+		alert('Please Provide valid cost.');
+		costPerPlayerField.value = '';
+	} else {
+		totalPlayerCost = calculatePlayerCost(totalPlayer, costPerPlayer);
+		totalPlayerCostField.innerText = totalPlayerCost;
+	}
+});
